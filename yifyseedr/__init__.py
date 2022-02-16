01ffafdcea02456244c3a1a00c4184691cd9def1
zafatar <zaferkiziltoprak@gmail.com>
2022-02-16 14:12:35 +0100

    add new config and client along with the requirement

--- yifyseedr/__init__.py
+++ yifyseedr/__init__.py
@@ -1,5 +1,6 @@
 
-from .Film import Film, Torrent
-from .Seedr import Seedr
-from .Yify import Yify, MAX_NUMBER
+from .models.Film import Film
+from .models.Torrent import Torrent
 
+from .services.Seedr import Seedr
+from .services.Yify import Yify, MAX_NUMBER
