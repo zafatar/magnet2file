206852a05f3e997f0b783f519cc6029ebd86cd08
zafatar <zaferkiziltoprak@gmail.com>
2022-02-16 14:10:10 +0100

    services added with the new format and library

---
+++ yifyseedr/services/Seedr.py
@@ -0,0 +1,176 @@
+# Seedr.py
+
+import requests
+import sys
+
+from yifyseedr.services import Services
+
+from utils.utils import pprint_dict
+
+
+class Seedr:
+    """
+    Seedr class
+    """
+    CODE = Services.SEEDR
+
+    def __init__(self, data):
+        self.email = data.get('email')
+        self.password = data.get('password')
+
+    def run(self):
+        available_actions = {
+            1: 'Download',
+            2: 'Delete'
+        }
+
+        available_folders = {
+            1: '/media/pi/DEPO_1/share/Movies/',
+            2: '/media/pi/DEPO_1/share/Series/',
+            3: 'Enter the folder manually...'
+        }
+
+        print("Do you want to download or delete file?")
+        for index, action in available_actions.items():
+            print(f"  {index} - {action}")
+
+        selected_action = input("\nDownload or Delete [1..2]: ")
+
+        # list folders.
+        folder_dict = self.get_folders_list()
+
+        if int(selected_action) == 1:
+            print(f"\nSelected action: {available_actions.get(int(selected_action))}")
+
+            Seedr.print_folders(folder_dict)
+
+            folder_number = input(f"\nSelect a folder for details [1..{len(folder_dict)}] :")
+            selected_folder = folder_dict[int(folder_number)]
+
+            pprint_dict(selected_folder)
+
+            folder_dict = self.get_folders_list(folder_id=selected_folder.get('id'))
+            Seedr.print_folders(folder_dict)
+
+            file_number = input(f"\nSelect a file to download [1..{len(folder_dict)}] :")
+            selected_file = folder_dict[int(file_number)]
+
+            pprint_dict(selected_file)
+
+            print("Where to download the files?")
+            for index, folder in available_folders.items():
+                print(f"  {index} - {folder}")
+
+            selected_action = input("\nSelect or enter folder [1..3]: ")
+
+            selected_folder = None
+            if int(selected_action) == 1 or int(selected_action) == 2:
+                selected_folder = available_folders.get(int(selected_action))
+            else:
+                selected_folder = input("\nEnter the folder location (ex. /media/pi/): ")
+
+            self.get_file(file_id=selected_file.get('id'),
+                          file_name=selected_file.get('name'),
+                          folder=selected_folder)
+
+        elif int(selected_action) == 2:
+            folder_number = input(f"Select a folder to delete [1..{len(folder_dict)}] :")
+            selected_folder = folder_dict[int(folder_number)]
+            self.delete_folder(folder_id=selected_folder.get('id'))
+
+    def add_file_from_magnet(self, magnet_link):
+        url = "https://www.seedr.cc/rest/torrent/magnet"
+        auth = (self.email, self.password)
+        response = requests.post(url, data={'magnet': magnet_link}, auth=auth)
+
+        if response.status_code != 200:
+            print("Error occurred: " + response.content)
+
+        ret = response.json()
+        print(ret)
+        return ret.get('user_torrent_id')
+
+    def get_folders_list(self, folder_id=None):
+        url = "https://www.seedr.cc/rest/folder"
+        if folder_id is not None:
+            url = url + "/" + str(folder_id)
+
+        auth = (self.email, self.password)
+
+        response = requests.get(url, auth=auth)
+
+        if response.status_code != 200:
+            print("Error occurred: " + response.content)
+
+        ret = response.json()
+
+        folders = {}
+        count = 1
+        for folder in ret.get('folders'):
+            folders[count] = folder
+            count += 1
+
+        for folder in ret.get('files'):
+            folders[count] = folder
+            count += 1
+
+        return folders
+
+    def get_file(self, file_id=None, file_name=None, folder='.'):
+        url = "https://www.seedr.cc/rest/file"
+        if file_id is not None:
+            url = url + "/" + str(file_id)
+
+        auth = (self.email, self.password)
+
+        with open(folder + '/' + file_name, 'wb') as f:
+            response = requests.get(url, auth=auth, stream=True)
+            total = response.headers.get('content-length')
+
+            if total is None:
+                f.write(response.content)
+            else:
+                downloaded = 0
+                total = int(total)
+                for data in response.iter_content(chunk_size=max(int(total/1000), 1024*1024)):
+                    downloaded += len(data)
+                    f.write(data)
+                    done = int(50*downloaded/total)
+                    sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50-done)))
+                    sys.stdout.flush()
+            sys.stdout.write('\n')
+
+        print(f"\nFile `{file_name}` downloaded into `{folder}.`")
+
+        return
+
+    def delete_folder(self, folder: dict = {}):
+        url = "https://www.seedr.cc/rest/folder"
+
+        if 'id' in folder and folder.get('id') is not None:
+            folder_id = folder.get('id')
+            url = url + "/" + str(folder_id)
+
+        auth = (self.email, self.password)
+
+        response = requests.delete(url, auth=auth)
+
+        if response.status_code != 200:
+            print("Error occurred while deleting folder: " + response.content)
+
+        print(f"\nFolder {folder.get('name')} deleted.\n")
+
+    @staticmethod
+    def print_folders(folder_dict):
+        max_title_length = 0
+        for count, folder in folder_dict.items():
+            if len(folder.get('name')) > max_title_length:
+                max_title_length = len(folder.get('name'))
+
+        for count, folder in folder_dict.items():
+            print("{} - [{}] - {}".format(str(count).rjust(3),
+                                          folder.get('id'),
+                                          str(folder.get('name')).ljust(max_title_length)))
+
+    def __repr__(self):
+        return '<Seedr.email {}>'.format(self.email)
---
+++ yifyseedr/services/Yify.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+#
+# services/Yify.py
+#
+import requests
+from urllib.parse import quote
+from lxml import html
+from utils.utils import update_progress
+
+from yifyseedr.services import Services
+from yifyseedr.services.Seedr import Seedr
+from yifyseedr.models.Film import Film
+from yifyseedr.models.Torrent import Torrent
+
+MAIN_URL = "https://yts.mx"
+MAX_NUMBER = 20
+
+
+class Yify:
+    """
+    Yify class
+    """
+    CODE = Services.YIFY
+    seedr_service = None
+
+    def __init__(self, seedr_service: Seedr = None):
+        self.seedr_service = seedr_service
+
+    def run(self):
+        search_string = input("Search in Yify: ")
+
+        # TODO: Sanitize the search string
+        film_dict = Yify.search_movies(search_string)
+
+        if film_dict is not None:
+            print(f"\n{len(film_dict)} found for `{search_string}`\n")
+            Yify.print_films(film_dict)
+
+            film_number = input(f"\nSelect a movie for details [1..{len(film_dict)}]: ")
+            selected_film = film_dict[int(film_number)]
+
+            for torrent in selected_film.torrents:
+                print(torrent)
+                if torrent.type.startswith('1080p'):
+                    self.seedr_service.add_file_from_magnet(torrent.magnet)
+        else:
+            print(f"No film found for `{search_string}`\n")
+
+    @staticmethod
+    def search_movies(search_string):
+        search_url = MAIN_URL + "/browse-movies/" + quote(search_string) + "/all/all/0/latest/0/all"
+        response = requests.get(search_url)
+        tree = html.fromstring(response.content)
+
+        # How many movies found :
+        number_of_movies = 0
+        # movies_found = tree.xpath('//h2/span[text()[contains(., "Movies found")]]/text()')
+        movies_found = tree.xpath('//h2/b/text()')
+
+        if movies_found[0] is not None:
+            pieces = movies_found[0].split(' ')
+            number_of_movies = int(pieces[0])
+
+        films = tree.xpath('//div[contains(@class, "browse-movie-wrap")]')
+
+        count = 1
+        film_dict = {}
+        for film_node in films:
+            if count > MAX_NUMBER:
+                continue
+
+            title = film_node.xpath('.//a[@class="browse-movie-title"]/text()')
+            link = film_node.xpath('.//a[@class="browse-movie-link"]/@href')
+            year = film_node.xpath('.//div[@class="browse-movie-year"]/text()')
+            rating = film_node.xpath('.//h4[@class="rating"]/text()')
+
+            film = Film(title=title[0],
+                        link=link[0],
+                        year=year[0],
+                        rating=rating[0])
+
+            film_details = requests.get(link[0])
+            film_details_tree = html.fromstring(film_details.content)
+
+            # print("{} - [{}] - {} - {}".format(str(count).rjust(3), film.year, film.title, film.link))
+
+            torrents = film_details_tree.xpath('.//div[@class="modal-torrent"]')
+            for torrent in torrents:
+                torrent_type = torrent.xpath('.//div[@class="modal-quality"]//text()')
+                torrent_size = torrent.xpath('.//p[@class="quality-size"]/text()')
+                magnet_link = torrent.xpath('.//a[contains(@class, "magnet")]//@href')
+
+                torrent = Torrent(type=torrent_type[0], size=torrent_size[1], magnet=magnet_link)
+                film.torrents.append(torrent)
+
+                # print("{} - {} - {}".format(str(torrent_type[0]).rjust(10), torrent_size[1], magnet_link))
+                # result = seedr.add_file_from_magnet(magnet_link)
+                # print("{} - {}", str(result['user_torrent_id']), result['title'])
+                # print("{} - {}".format(str(torrent_type[0]).rjust(10), torrent_size[1]))
+
+            # print("".join(["-"] * 100))
+
+            film_dict[count] = film
+            update_progress(count, number_of_movies)
+            count += 1
+
+        return film_dict
+
+    @staticmethod
+    def get_latest_films(page_number=1):
+        browse_url = MAIN_URL + "/browse-movies?page=" + str(page_number)
+        response = requests.get(browse_url)
+        tree = html.fromstring(response.content)
+
+        films = tree.xpath('//div[contains(@class, "browse-movie-wrap")]')
+
+        count = 1
+        film_dict = {}
+        for film_node in films:
+            if count > MAX_NUMBER:
+                continue
+
+            title = film_node.xpath('.//a[@class="browse-movie-title"]/text()')
+            link = film_node.xpath('.//a[@class="browse-movie-link"]/@href')
+            year = film_node.xpath('.//div[@class="browse-movie-year"]/text()')
+            rating = film_node.xpath('.//h4[@class="rating"]/text()')
+
+            film = Film(title=title[0], link=link[0], year=year[0], rating=rating[0])
+
+            film_details = requests.get(link[0])
+            film_details_tree = html.fromstring(film_details.content)
+
+            # print("{} - [{}] - {} - {}".format(str(count).rjust(3), film.year, film.title, film.link))
+
+            torrents = film_details_tree.xpath('.//div[@class="modal-torrent"]')
+            for torrent in torrents:
+                torrent_type = torrent.xpath('.//div[@class="modal-quality"]//text()')
+                torrent_size = torrent.xpath('.//p[@class="quality-size"]/text()')
+                magnet_link = torrent.xpath('.//a[contains(@class, "magnet")]//@href')
+
+                torrent = Torrent(type=torrent_type[0], size=torrent_size[1], magnet=magnet_link)
+                film.torrents.append(torrent)
+
+                # print("{} - {} - {}".format(str(torrent_type[0]).rjust(10), torrent_size[1], magnet_link))
+                # result = seedr.add_file_from_magnet(magnet_link)
+                # print("{} - {}", str(result['user_torrent_id']), result['title'])
+                # print("{} - {}".format(str(torrent_type[0]).rjust(10), torrent_size[1]))
+
+            # print("".join(["-"] * 100))
+
+            film_dict[count] = film
+            update_progress(count * int(100 / MAX_NUMBER))
+            count += 1
+
+        return film_dict
+
+    @staticmethod
+    def print_films(film_dict):
+        max_title_length = 0
+        for count, film in film_dict.items():
+            if len(film.title) > max_title_length:
+                max_title_length = len(film.title)
+
+        for count, film in film_dict.items():
+            print("{} - [{}] - [{}] - {} - {}".format(str(count).rjust(3),
+                                                      film.year,
+                                                      str(film.rating).rjust(8),
+                                                      str(film.title).ljust(max_title_length),
+                                                      film.link))
